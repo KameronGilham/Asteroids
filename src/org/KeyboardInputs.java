63661e788230656bc1fb23414ab0712b0369bcb4
kjgilham <kjgilham@Cisco-04.anc.local>
2019-04-02 14:52:24 -0700

    Commit #3

---
+++ src/org/KeyboardInputs.java
@@ -0,0 +1,49 @@
+package org;
+
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+
+public class KeyboardInputs {
+	
+	boolean shipFWD = false;
+	boolean shipBACK = false;
+	boolean shipSpinL = false;
+	boolean shipSpinR = false;
+	
+
+	public void keyboardInput(KeyEvent key, GraphicsContext gc) {
+		if (key.getEventType() == KeyEvent.KEY_PRESSED) {
+			if (key.getCode() == KeyCode.UP) {
+				shipFWD = true;
+			}
+			if (key.getCode() == KeyCode.DOWN) {
+				shipBACK = true;
+			}
+			if (key.getCode() == KeyCode.LEFT) {
+				shipSpinL = true;
+			}
+			if (key.getCode() == KeyCode.RIGHT) {
+				shipSpinR = true;
+			}
+			
+		}
+		if (key.getEventType() == KeyEvent.KEY_RELEASED) {
+			if (key.getCode() == KeyCode.UP) {
+				shipFWD = false;
+			}
+			if (key.getCode() == KeyCode.DOWN) {
+				shipBACK = false;
+			}
+			if (key.getCode() == KeyCode.LEFT) {
+				shipSpinL = false;
+			}
+			if (key.getCode() == KeyCode.RIGHT) {
+				shipSpinR = false;
+			}
+		}
+		
+
+	}
+
+}
--- src/org/Main.java
+++ src/org/Main.java
@@ -1,10 +1,12 @@
 package org;
 
+import javafx.application.Application;
+
 public class Main {
 	
 	public static void main(String args[]) {
 		
-	
+		Application.launch(AsteroidsGame.class, args);
 		
 	}
 
