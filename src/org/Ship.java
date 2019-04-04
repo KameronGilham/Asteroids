1a28eecaa506bfe2cdc3ec3f8658cd780f3a787f
kjgilham <kjgilham@Cisco-04.anc.local>
2019-04-04 10:38:24 -0700

    Commit #9

--- src/org/Ship.java
+++ src/org/Ship.java
@@ -6,7 +6,7 @@ import javafx.scene.transform.Affine;
 import javafx.scene.transform.Rotate;
 
 public class Ship {
-	
+
 	Color color;
 	double degrees;
 	double shipX;
@@ -14,71 +14,77 @@ public class Ship {
 	double xVelocity;
 	double yVelocity;
 	double acceleration;
-	double angle = (degrees * (Math.PI/180));
-	
-public Ship() {
-	
-	color = Color.WHITE;
-	shipX = AsteroidsGame.canvasW/2;
-	shipY = AsteroidsGame.canvasH/2;
-	degrees = 0;
-	acceleration = .1; 
-	xVelocity = 0;
-	yVelocity = 0;
-	
-}
+	double angle;
 
-public void draw(GraphicsContext gc) {
-	
-	double[] xPoints = new double[]{shipX, shipX, shipX - 25, shipX + 25, shipX};
-	double[] yPoints = new double[]{shipY, shipY - 33.33, shipY + 16.66, shipY + 16.66, shipY - 33.33};
-	
+	public Ship() {
+
+		color = Color.WHITE;
+		shipX = AsteroidsGame.canvasW / 2;
+		shipY = AsteroidsGame.canvasH / 2;
+		degrees = 0;
+		acceleration = 1;
+		xVelocity = 0;
+		yVelocity = 0;
+
+	}
+
+	public void draw(GraphicsContext gc) {
+
+		double[] xPoints = new double[] { shipX, shipX, shipX - 25, shipX, shipX + 25, shipX };
+		double[] yPoints = new double[] { shipY, shipY - 33.33, shipY + 16.66, shipY + 5, shipY + 16.66, shipY - 33.33 };
+
 		gc.setFill(color);
-		
+
 		gc.save();
-		gc.transform(new Affine(new Rotate(degrees,shipX,shipY)));
-		gc.fillPolygon(xPoints, yPoints, 5);
+		gc.transform(new Affine(new Rotate(degrees, shipX, shipY)));
+		gc.fillPolygon(xPoints, yPoints, 6);
 		gc.restore();
 	}
 
-
 	public void update(KeyboardInputs keyboarding, GraphicsContext gc) {
-		
-		if(KeyboardInputs.shipSpinR) {
+
+		if (KeyboardInputs.shipSpinR) {
 			degrees++;
 		}
-		
-		if(KeyboardInputs.shipSpinL) {
+
+		if (KeyboardInputs.shipSpinL) {
 			degrees--;
 		}
-		
+
 		if (degrees > 360) {
 			degrees -= 360;
-			
-		}else if (degrees < 0) {
+
+		} else if (degrees < 0) {
 			degrees += 360;
 		}
 		
-		if(KeyboardInputs.shipFWD) {
+		angle = (degrees * (Math.PI / 180));
+
+		if (KeyboardInputs.shipFWD) {
+
+			xVelocity = Math.sin(angle) * acceleration;
+			yVelocity = Math.cos(angle) * acceleration;
+
+			shipX += xVelocity;
+
+			shipY -= yVelocity;
 			
-		  xVelocity += Math.sin(angle) * acceleration;
-		  yVelocity += Math.cos(angle) * acceleration;
-		  
-		  if(angle > Math.PI/2 && angle < 3 * Math.PI/2) {
-			  yVelocity *= (-1);
-		  }
-		  if(angle > Math.PI && angle < 2 * Math.PI) {
-			  xVelocity *= (-1);
-		  }
-			  
-		  
-		  shipX += xVelocity;
-		  
-		  shipY -= yVelocity;
-		  
-		  
+			acceleration += .05;
+
 		}
-	
+		if (shipX < 0) {
+			shipX += AsteroidsGame.canvasW;
+		}else if (shipX > AsteroidsGame.canvasW) {
+			shipX -= AsteroidsGame.canvasH;
+		}
+		
+		if (shipY < 0) {
+			shipY += AsteroidsGame.canvasH;
+		}else if (shipY > AsteroidsGame.canvasH) {
+			shipY -= AsteroidsGame.canvasH;
+			
+		}
+
 	}
 
 }
